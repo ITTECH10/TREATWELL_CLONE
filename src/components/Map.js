f0709536330a85bf2502fffd721bbc00de0627c4
ITTECH10 <ittech.a1development@gmail.com>
2022-03-23 10:42:52 +0100

    minor change

--- src/components/Map.js
+++ src/components/Map.js
@@ -11,7 +11,7 @@ const Map = ({ lng, lat }) => {
     return (
         <ReactMapGL
             mapStyle="mapbox://styles/mapbox/streets-v11?optimize=true"
-            mapboxApiAccessToken='pk.eyJ1IjoidHJlYXR3ZWxsMTAiLCJhIjoiY2t5bXd6dnVrMDF3NTJ2b3k0Y20zMDR5dCJ9.wIzLh0qg1SiCZ3mm8t9qug'
+            mapboxApiAccessToken={token}
             {...viewport}
             height="400px" width="100%"
             onViewportChange={setViewport}
