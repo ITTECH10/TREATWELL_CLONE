d492bfeea40c1e2c335fd354c5fbf2f4bd991c32
ITTECH10 <ittech.a1development@gmail.com>
2022-03-28 22:49:20 +0200

    changed some link hrefs

--- src/components/_home/HomeWelcomeVideo.js
+++ src/components/_home/HomeWelcomeVideo.js
@@ -23,10 +23,10 @@ const HomeWelcomeVideo = () => {
                 <Box sx={{ width: { xs: '100%', md: '45%' }, ml: { xs: 0, md: 2 }, p: { xs: 2, md: 0 } }}>
                     <Stack direction="row" justifyContent="center" alignItems="center" sx={{ height: { xs: 230, md: '100%' }, position: 'relative' }}>
                         <Typography variant="h6" sx={{ textAlign: 'center', position: 'absolute', width: '100%', mb: { xs: '10rem', md: '9rem' } }}>Folgen Sie uns auf Facebook und Instagram für neueste Angebote und Informationen!</Typography>
-                        <IconButton href="https://facebook.com" target="_blank" sx={{ color: '#3b5998' }}>
+                        <IconButton href="https://www.facebook.com/Gesundo24-105847441995637/" target="_blank" sx={{ color: '#3b5998' }}>
                             <FacebookIcon sx={{ fontSize: { xs: '4rem', md: '6rem' } }} />
                         </IconButton>
-                        <IconButton href="https://instagram.com" target="_blank" sx={{ color: '#8a3ab9' }}>
+                        <IconButton href="https://instagram.com/gesundo24.de?utm_medium=copy_link" target="_blank" sx={{ color: '#8a3ab9' }}>
                             <InstagramIcon sx={{ fontSize: { xs: '4rem', md: '6rem' } }} />
                         </IconButton>
                     </Stack>
